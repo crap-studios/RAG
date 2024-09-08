13b8a75c8e7bd3de417cf83bbd04e9557baa21b6
Manan Garg <manantechy@gmail.com>
2024-09-08 19:32:21 +0530

    feat: method to hit image generation api

---
+++ frontend/src/services/api/imageApi.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const sendImagePrompt = async (
+    prompt: string
+) => {
+    const data = axios.post(
+        `https://9261-2401-4900-8838-6e3d-1b70-8d1e-a8bd-8b4d.ngrok-free.app/api/v1/image/`,
+        {
+            prompt: prompt
+        },
+    );
+    return data;
+}
+
+export default sendImagePrompt;

e0c6d3778214a1cbcbb18d5c21c523353a2c426c
Manan Garg <manantechy@gmail.com>
2024-09-08 20:26:05 +0530

    feat: integrate image generation API

--- frontend/src/services/api/imageApi.ts
+++ frontend/src/services/api/imageApi.ts
@@ -12,4 +12,13 @@ const sendImagePrompt = async (
     return data;
 }
 
-export default sendImagePrompt;
+const sendImageGenerationSignedTx = async (
+    signedTx: string
+) => {
+    axios.post(`https://9261-2401-4900-8838-6e3d-1b70-8d1e-a8bd-8b4d.ngrok-free.app/api/v1/image/generate`, {
+        signed_txn: signedTx
+    },);
+    return;
+}
+
+export { sendImagePrompt, sendImageGenerationSignedTx };

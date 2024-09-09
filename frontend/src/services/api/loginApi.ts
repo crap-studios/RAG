99699b5ecbeebc16b156272247c2374ce07dc347
Manan Garg <manantechy@gmail.com>
2024-09-09 21:25:27 +0530

    feat: set tokens after logging in

--- frontend/src/services/api/loginApi.ts
+++ frontend/src/services/api/loginApi.ts
@@ -4,7 +4,7 @@ const login = async (
   message: string,
   signedMessage: string
 ) => {
-  const data = axios.post(
+  const data = await axios.post(
     `https://9261-2401-4900-8838-6e3d-1b70-8d1e-a8bd-8b4d.ngrok-free.app/api/v1/auth/login/`,
     {
       address: address,
@@ -12,6 +12,9 @@ const login = async (
       signature: signedMessage,
     }
   );
+  // store tokens in local storage
+  localStorage.setItem("accessToken", data.data["data"]["access_token"]);
+  localStorage.setItem("refreshToken", data.data["data"]["refresh_token"]);
   return data;
 };
 
